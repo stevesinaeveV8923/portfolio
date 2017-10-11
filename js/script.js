eb46079dbc873cd5dc188a58f9cf6c224930fb3f
Steve Sinaeve <stevesinaeve@MacBook-Air-van-Steve.local>
2017-10-11 15:12:03 +0200

    update website

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
-// KLOPT NOG NIET HELEMAAL!!!!
+// // KLOPT NOG NIET HELEMAAL!!!!
 
 $('.abouth').click(function(){
 
     $(this).toggleClass("active");
-    $('a').css('color', 'white');
     $('.xph').removeClass('active');
     $('.contacth').removeClass('active');
-    $('#about').toggleClass("toggle");
+
     if($("#about").css('display') === 'none')
     {
+        // $('.xph').css('color','white');
+        // $('.contacth').css('color','white');
         $('#xp').css('display','none');
         $('#contact').css('display','none');
         $('.headTitle').css('display','none');
-        $('#about').show();
+        $('#about').css('display', 'initial');
+        $('#about').addClass('toggle');
         $('.content').css('top', '100%');
         $('.content').animate({top: '15%' } , "medium");
     }
     else 
     {
+        // $('.xph').css('color','#40C4AA');
+        // $('.contacth').css('color','#40C4AA');
         $('#about').css('display', 'none');
         $('#contact').css('display', 'none');
         $('#xp').css('display','none');
         $('#about').css('display','none');
+        $('#about').removeClass('toggle');
         $('.headTitle').css('display','inline-block');
     }
 });
@@ -32,20 +37,23 @@ $('.xph').click(function(){
     $(this).toggleClass("active");
     $('.abouth').removeClass('active');
     $('.contacth').removeClass('active');
-    $('#xp').toggleClass("toggle");
     if($("#xp").css('display') === 'none')
     {
         $('#contact').css('display','none');
         $('#about').css('display','none');
         $('.headTitle').css('display','none');
+        // $('a').css('color', 'white');
+        $('#xp').addClass('toggle');
         $('#xp').show();
     }
     else 
     {
+        // $('a').css('color', '#40C4AA');
         $('#xp').css('display', 'none');
         $('#contact').css('display', 'none');
         $('#xp').css('display','none');
         $('#about').css('display','none');
+        $('#xp').removeClass('toggle');
         $('.headTitle').css('display','inline-block');
     }
 });
@@ -56,19 +64,22 @@ $('.contacth').click(function(){
     $(this).toggleClass("active");
     $('.xph').removeClass('active');
     $('.abouth').removeClass('active');
-    $('#contact').toggleClass("toggle");
     if($("#contact").css('display') === 'none')
     {
         $('#about').css('display','none');
         $('#xp').css('display','none');
         $('.headTitle').css('display','none');
+        // $('a').css('color', 'white');
+        $('#contact').addClass('toggle');
         $('#contact').show();
     }
     else 
     {
+        // $('a').css('color', '#40C4AA');
         $('#contact').css('display', 'none');
         $('#xp').css('display','none');
         $('#about').css('display','none'); 
+        $('#contact').removeClass('toggle');
         $('.headTitle').css('display','inline-block');
     }
 });
